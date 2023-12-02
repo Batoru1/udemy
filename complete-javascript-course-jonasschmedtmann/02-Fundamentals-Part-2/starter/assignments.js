de7f917fa3bec1c6b77690b666c8674bf29976b0
batoru1 <103144015+Batoru1@users.noreply.github.com>
2023-12-02 16:46:45 +0200

    while loop

--- complete-javascript-course-jonasschmedtmann/02-Fundamentals-Part-2/starter/assignments.js
+++ complete-javascript-course-jonasschmedtmann/02-Fundamentals-Part-2/starter/assignments.js
@@ -1,8 +1,27 @@
 'use strict';
+
 //48 looping backwards and loops in loops
 
-const listofNeighbours = [
-  ['Canada', 'Mexico'],
-  ['Spain'],
-  ['Norway', 'Sweden', 'Russia'],
-];
+// const listOfNeighbours = [
+//   ['Canada', 'Mexico'],
+//   ['Spain'],
+//   ['Norway', 'Sweden', 'Russia'],
+// ];
+// for (let i = 0; i < listOfNeighbours.length; i++)
+//   for (let y = 0; y < listOfNeighbours[i].length; y++)
+//     console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
+
+//49 the While Loop
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const populations = [10, 1441, 332, 83];
+const percentages2 = [];
+for (let i = 0; i < populations.length; i++) {
+  // const perc = percentageOfWorld1(populations[i]);
+  // percentages2.push(perc);
+  percentages2.push(percentageOfWorld1(populations[i]));
+}
+console.log(percentages2);
