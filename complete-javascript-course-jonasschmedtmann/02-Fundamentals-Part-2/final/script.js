77be9fadc981fe659f2510be4af54d1e54855f12
batoru1 <103144015+Batoru1@users.noreply.github.com>
2023-12-02 16:47:30 +0200

    while loop

--- complete-javascript-course-jonasschmedtmann/02-Fundamentals-Part-2/final/script.js
+++ complete-javascript-course-jonasschmedtmann/02-Fundamentals-Part-2/final/script.js
@@ -508,13 +508,13 @@ for (let i = jonas.length - 1; i >= 0; i--) {
   console.log(i, jonas[i]);
 }
 
-for (let exercise = 1; exercise < 4; exercise++) {
-  console.log(`-------- Starting exercise ${exercise}`);
+// for (let exercise = 1; exercise < 4; exercise++) {
+//   console.log(`-------- Starting exercise ${exercise}`);
 
-  for (let rep = 1; rep < 6; rep++) {
-    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
-  }
-}
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+//   }
+// }
 
 
 ///////////////////////////////////////
