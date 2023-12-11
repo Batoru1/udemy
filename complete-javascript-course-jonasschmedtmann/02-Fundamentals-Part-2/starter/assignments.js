1b4daaa9d8eb784f42e2e22cdb051152d674fd64
Tomas <103144015+Batoru1@users.noreply.github.com>
2023-12-11 19:43:15 +0200

    started repeating coding challanges complete-javascript-course-jonasschmedtmann/02-Fundamentals-Part-2/starter/assignments.js

--- complete-javascript-course-jonasschmedtmann/02-Fundamentals-Part-2/starter/assignments.js
+++ complete-javascript-course-jonasschmedtmann/02-Fundamentals-Part-2/starter/assignments.js
@@ -231,15 +231,15 @@
 
 //functions
 
-function describeCountry(country, population, capitalCity) {
-  console.log(
-    `${country} has ${population} million people and it's capital city is ${capitalCity}`
-  );
-}
+// function describeCountry(country, population, capitalCity) {
+//   console.log(
+//     `${country} has ${population} million people and it's capital city is ${capitalCity}`
+//   );
+// }
 
-const countryLit = describeCountry('Lithuania', 2.8, 'Vilnius');
-const countryPol = describeCountry('Poland', 38, 'Warsaw');
-const countryLat = describeCountry('Latvia', 1.8, 'Ryga');
+// const countryLit = describeCountry('Lithuania', 2.8, 'Vilnius');
+// const countryPol = describeCountry('Poland', 38, 'Warsaw');
+// const countryLat = describeCountry('Latvia', 1.8, 'Ryga');
 
 // function describeCountry(country, population, capitalCity) {
 //   return `${country} has ${population} million people and it's capital city is ${capitalCity}`;
@@ -253,176 +253,292 @@ const countryLat = describeCountry('Latvia', 1.8, 'Ryga');
 
 //Function Declarations vs. Expressions
 
-function percentageOfWorld1(population) {
-  return (population / 7900) * 100;
-}
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
 
-const popLit = percentageOfWorld1(2.8);
-const popLat = percentageOfWorld1(1.8);
-const popEst = percentageOfWorld1(1.3);
+// const popLit = percentageOfWorld1(2.8);
+// const popLat = percentageOfWorld1(1.8);
+// const popEst = percentageOfWorld1(1.3);
 
-console.log(popLit, popLat, popEst);
+// console.log(popLit, popLat, popEst);
 
-const percentageOfWorld2 = function (population) {
-  return (population / 7900) * 100;
-};
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
 
-const popPol = percentageOfWorld2(38);
-const popGer = percentageOfWorld2(83);
-const popUsa = percentageOfWorld2(331);
+// const popPol = percentageOfWorld2(38);
+// const popGer = percentageOfWorld2(83);
+// const popUsa = percentageOfWorld2(331);
 
-console.log(popPol, popGer, popUsa);
+// console.log(popPol, popGer, popUsa);
 
-//Arrow function
-const percentageOfWorld3 = (population) => (population / 7900) * 100;
+// //Arrow function
+// const percentageOfWorld3 = (population) => (population / 7900) * 100;
 
-const popTaiw = percentageOfWorld3(23);
-const popDen = percentageOfWorld3(5.8);
-const popUkr = percentageOfWorld3(41);
+// const popTaiw = percentageOfWorld3(23);
+// const popDen = percentageOfWorld3(5.8);
+// const popUkr = percentageOfWorld3(41);
 
-console.log(popTaiw, popDen, popUkr);
+// console.log(popTaiw, popDen, popUkr);
 
 //Functions calling other functions
 
 //declaration
-function describePopulation(country, population) {
-  return `${country} has ${population} million people, which is about ${percentageOfWorld1(
-    population
-  )}% of world population`;
-}
+// function describePopulation(country, population) {
+//   return `${country} has ${population} million people, which is about ${percentageOfWorld1(
+//     population
+//   )}% of world population`;
+// }
 
-console.log(describePopulation('Finland', 5.5));
-console.log(describePopulation('Japan', 126));
-console.log(describePopulation('Netherlands', 17.5));
+// console.log(describePopulation('Finland', 5.5));
+// console.log(describePopulation('Japan', 126));
+// console.log(describePopulation('Netherlands', 17.5));
 
 //expression
-const describePopulation2 = function (country, population) {
-  return `${country} has ${population} million people, which is about ${percentageOfWorld1(
-    population
-  )}% of world population`;
-};
+// const describePopulation2 = function (country, population) {
+//   return `${country} has ${population} million people, which is about ${percentageOfWorld1(
+//     population
+//   )}% of world population`;
+// };
 
-console.log(describePopulation2('Finland', 5.5));
-console.log(describePopulation2('Japan', 126));
-console.log(describePopulation2('Netherlands', 17.5));
+// console.log(describePopulation2('Finland', 5.5));
+// console.log(describePopulation2('Japan', 126));
+// console.log(describePopulation2('Netherlands', 17.5));
 
 //arrow
-const describePopulation3 = (country, population) =>
-  `${country} has ${population} million people, which is about ${percentageOfWorld1(
-    population
-  )}% of world population`;
+// const describePopulation3 = (country, population) =>
+//   `${country} has ${population} million people, which is about ${percentageOfWorld1(
+//     population
+//   )}% of world population`;
 
-console.log(describePopulation3('Finland', 5.5));
-console.log(describePopulation3('Japan', 126));
-console.log(describePopulation3('Netherlands', 17.5));
+// console.log(describePopulation3('Finland', 5.5));
+// console.log(describePopulation3('Japan', 126));
+// console.log(describePopulation3('Netherlands', 17.5));
 
 //Introduction to arrays
 
-const populations = [2.8, 33, 331, 23];
+// const populations = [2.8, 33, 331, 23];
 
-console.log(populations.length === 4);
+// console.log(populations.length === 4);
 
-const percentages = [
-  percentageOfWorld1(populations[0]),
-  percentageOfWorld1(populations[1]),
-  percentageOfWorld1(populations[2]),
-  percentageOfWorld1(populations[3]),
-];
+// const percentages = [
+//   percentageOfWorld1(populations[0]),
+//   percentageOfWorld1(populations[1]),
+//   percentageOfWorld1(populations[2]),
+//   percentageOfWorld1(populations[3]),
+// ];
 
-console.log(percentages);
+// console.log(percentages);
 
 //basic array operations(methods)
 
-const neighboursLit = ['Latvia', 'Poland', 'Belarus', 'Russia'];
-console.log(neighboursLit);
+// const neighboursLit = ['Latvia', 'Poland', 'Belarus', 'Russia'];
+// console.log(neighboursLit);
 
-neighboursLit.push('Utopia');
-console.log(neighboursLit);
+// neighboursLit.push('Utopia');
+// console.log(neighboursLit);
 
-neighboursLit.pop();
-console.log(neighboursLit);
+// neighboursLit.pop();
+// console.log(neighboursLit);
 
-!neighboursLit.includes('Germany')
-  ? console.log(' Probably not a central European country :D')
-  : console.log('A central European country');
+// !neighboursLit.includes('Germany')
+//   ? console.log(' Probably not a central European country :D')
+//   : console.log('A central European country');
 
-console.log(neighboursLit.indexOf('Russia'));
-neighboursLit[neighboursLit.indexOf('Russia')] = 'Orckland';
-console.log(neighboursLit);
+// console.log(neighboursLit.indexOf('Russia'));
+// neighboursLit[neighboursLit.indexOf('Russia')] = 'Orckland';
+// console.log(neighboursLit);
 
 //introduction to objects
 
-const myCountry = {
-  country: 'Lithuania',
-  capital: 'Vilnius',
-  language: 'lithuanian',
-  population: 2.8,
-  neighbours: ['Latvia', 'Poland', 'Belarus', 'Russia'],
-};
+// const myCountry = {
+//   country: 'Lithuania',
+//   capital: 'Vilnius',
+//   language: 'lithuanian',
+//   population: 2.8,
+//   neighbours: ['Latvia', 'Poland', 'Belarus', 'Russia'],
+// };
 
 //Dot vs. Bracket Notation
 
-console.log(
-  `${myCountry.country} has ${myCountry['population']} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry['capital']}`
-);
+// console.log(
+//   `${myCountry.country} has ${myCountry['population']} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry['capital']}`
+// );
 
-myCountry.population += 2;
-console.log(myCountry.population);
+// myCountry.population += 2;
+// console.log(myCountry.population);
 
-myCountry['population'] -= 2;
-console.log(myCountry.population);
+// myCountry['population'] -= 2;
+// console.log(myCountry.population);
 
 //Object methods
 
-myCountry.describe = function () {
-  console.log(
-    `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`
-  );
-};
+// myCountry.describe = function () {
+//   console.log(
+//     `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`
+//   );
+// };
 
-myCountry.describe();
+// myCountry.describe();
 
-myCountry.checkIsland = function () {
-  this.isIsland = !Boolean(this.neighbours.length);
-  // this.isIsland = this.neighbours.length === 0 ? true : false;
-  // console.log(this.sIsland);
-};
+// myCountry.checkIsland = function () {
+//   this.isIsland = !Boolean(this.neighbours.length);
+// this.isIsland = this.neighbours.length === 0 ? true : false;
+// console.log(this.sIsland);
+// };
 
-myCountry.checkIsland();
+// myCountry.checkIsland();
 
-console.log(myCountry);
+// console.log(myCountry);
 
 //iteration the for loop
 
-for (let i = 1; i <= 50; i++) {
-  console.log(`voter number ${i} is currently voting`);
-}
+// for (let i = 1; i <= 50; i++) {
+//   console.log(`voter number ${i} is currently voting`);
+// }
 
-//looping arrays breaking and continuing
-function percentageOfWorld12(population) {
-  return (population / 7900) * 100;
-}
+// //looping arrays breaking and continuing
+// function percentageOfWorld12(population) {
+//   return (population / 7900) * 100;
+// }
 
-const populations2 = [2.8, 33, 331, 23];
+// const populations2 = [2.8, 33, 331, 23];
 
-const percentages2 = [];
-for (let i = 0; i < populations2.length; i++) {
-  percentages2[i] = percentageOfWorld12(populations2[i]);
-}
+// const percentages2 = [];
+// for (let i = 0; i < populations2.length; i++) {
+//   percentages2[i] = percentageOfWorld12(populations2[i]);
+// }
 
-console.log(percentages2, percentages);
+// console.log(percentages2, percentages);
 
 //looping backwards and loops in loops
 
-const listOfNeighbours = [
-  ['Canada', 'Mexico'],
-  ['Spain'],
-  ['Norway', 'Sweden', 'Russia'],
-];
+// const listOfNeighbours = [
+//   ['Canada', 'Mexico'],
+//   ['Spain'],
+//   ['Norway', 'Sweden', 'Russia'],
+// ];
 
-for (let i = 0; i < listOfNeighbours.length; i++) {
-  for (let y = 0; y < listOfNeighbours[i].length; y++)
-    console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
-}
+// for (let i = 0; i < listOfNeighbours.length; i++) {
+//   for (let y = 0; y < listOfNeighbours[i].length; y++)
+//     console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
+// }
 
 //the while loop
+// const percentages3 = [];
+
+// let i = 0;
+// while (i < populations2.length) {
+//   percentages3[i] = percentageOfWorld12(populations2[i]);
+//   i++;
+// }
+
+// console.log(percentages, percentages2, percentages3);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////CODING CHALLANGES REPEAT/////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+//Coding challange 1
+
+const massMark1 = 78;
+const heightMark1 = 1.69;
+const massJohn1 = 92;
+const heightJohn1 = 1.95;
+
+const massMark2 = 95;
+const heightMark2 = 1.88;
+const massJohn2 = 85;
+const heightJohn2 = 1.76;
+
+const BMIMark1 = massMark1 / heightMark1 ** 2;
+const BMIJohn1 = massJohn2 / heightJohn1 ** 2;
+
+const BMIMark2 = massMark1 / heightMark2 ** 2;
+const BMIJohn2 = massJohn2 / heightJohn2 ** 2;
+
+console.log(BMIMark1, BMIJohn1, BMIMark2, BMIJohn2);
+
+const markHigherBMI1 = BMIMark1 > BMIJohn1;
+const markHigherBMI2 = BMIMark2 > BMIJohn2;
+
+console.log(markHigherBMI1, markHigherBMI2);
+
+//coding challange2
+
+if (BMIMark1 > BMIJohn1) {
+  console.log(`Mark's BMI(${BMIMark1}) is higher than John's(${BMIJohn1})!🐳`);
+} else {
+  console.log(`John's BMI(${BMIJohn1}) is higher than Mark's(${BMIMark1})! 🐋`);
+}
+
+if (BMIMark2 > BMIJohn2) {
+  console.log(`Mark's BMI(${BMIMark2}) is higher than John's(${BMIJohn2})!🐳`);
+} else {
+  console.log(`John's BMI(${BMIJohn2}) is higher than Mark's(${BMIMark2})!🐋`);
+}
+
+//coding challange 3
+const scoreDolphins1 = [96, 108, 89];
+const scoreKoalas1 = [88, 91, 110];
+
+const calcAvgD1 =
+  (scoreDolphins1[0] + scoreDolphins1[1] + scoreDolphins1[2]) / 3;
+
+const calcAvgK1 =
+  (scoreKoalas1[0] + scoreKoalas1[1] + scoreKoalas1[2]) / scoreKoalas1.length;
+
+console.log(calcAvgD1, calcAvgK1);
+
+if (calcAvgD1 > calcAvgK1) {
+  console.log(`Dolphins won ${calcAvgD1} to ${calcAvgK1}!
+  🏆 `);
+} else if (calcAvgD1 < calcAvgK1) {
+  console.log(`Koalas won ${calcAvgK1} to ${calcAvgD1}! 🏆`);
+} else {
+  console.log(`it's a tie ${calcAvgD1} = ${calcAvgK1}! 🟰`);
+}
+
+//Bonus 1
+const scoreDolphins2 = [97, 112, 101];
+const scoreKoalas2 = [109, 95, 123];
+
+const calcAvgD2 =
+  (scoreDolphins2[0] + scoreDolphins2[1] + scoreDolphins2[2]) / 3;
+
+const calcAvgK2 =
+  (scoreKoalas2[0] + scoreKoalas2[1] + scoreKoalas2[2]) / scoreKoalas1.length;
+
+console.log(calcAvgD2, calcAvgK2);
+
+if ((calcAvgD2 > calcAvgK2) & (calcAvgD2 >= 100)) {
+  console.log(`Dolphins won ${calcAvgD2} to ${calcAvgK2}!
+  🏆 `);
+} else if ((calcAvgD2 < calcAvgK2) & (calcAvgK2 >= 100)) {
+  console.log(`Koalas won ${calcAvgK2} to ${calcAvgD2}! 🏆`);
+} else {
+  console.log(`it's a tie ${calcAvgD2} = ${calcAvgK2}! 🟰`);
+}
+
+//Bonus 2
+const scoreDolphins3 = [97, 112, 101];
+const scoreKoalas3 = [109, 95, 106];
+
+const calcAvgD3 =
+  (scoreDolphins3[0] + scoreDolphins3[1] + scoreDolphins3[2]) / 3;
+
+const calcAvgK3 =
+  (scoreKoalas3[0] + scoreKoalas3[1] + scoreKoalas3[2]) / scoreKoalas1.length;
+
+console.log(calcAvgD3, calcAvgK3);
+
+if ((calcAvgD3 > calcAvgK3) & (calcAvgD3 >= 100)) {
+  console.log(`Dolphins won ${calcAvgD3} to ${calcAvgK3}!
+  🏆 `);
+} else if ((calcAvgD3 < calcAvgK3) & (calcAvgK3 >= 100)) {
+  console.log(`Koalas won ${calcAvgK3} to ${calcAvgD3}! 🏆`);
+} else if ((calcAvgD3 === calcAvgK3) & (calcAvgD3 >= 100)) {
+  console.log(`it's a tie ${calcAvgD3} = ${calcAvgK3}! 🟰`);
+} else {
+  console.log(`no one wins ${calcAvgD3} & ${calcAvgK3} less than 100! ☠️`);
+}
