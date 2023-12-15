d68367111d74505f0b7e4488d6ebe4ceb134e5d2
Tomas <103144015+Batoru1@users.noreply.github.com>
2023-12-15 09:29:59 +0200

    complete-javascript-course-jonasschmedtmann/03-Developer-Skills/starter/script.js

--- complete-javascript-course-jonasschmedtmann/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+const calcAge = birthYear => 2037 - birthYear;
+
+const calcTip = anyBill =>
+  (50 <= anyBill) & (anyBill <= 300) ? anyBill * 0.15 : anyBill * 0.2;
+
+console.log();
+console.log(calcAge);
+
+console.log();
+
+//video
