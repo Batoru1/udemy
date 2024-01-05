8d1d762ea977ace93340cb8547978ee98f572959
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-01-05 23:11:05 +0200

    dom chalange 1

--- complete-javascript-course-jonasschmedtmann/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/05-Guess-My-Number/starter/script.js
@@ -12,16 +12,20 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🛑 No number';
+    //when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👾 Correct number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    //guess is to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🔼 Too high!';
@@ -31,6 +35,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🤯 you lost the game';
       document.querySelector('.score').textContent = 0;
     }
+    //guess is to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🔽 Too low!';
@@ -41,4 +46,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.guess').
+  
+  ).textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('.number').style.width = '15rem';
 });
