dc7e870b72523ffa18e2f34fe0b68472d622f170
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-01-23 19:38:48 +0200

    toggle

--- complete-javascript-course-jonasschmedtmann/07-Pig-Game/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const diceEl = document.querySelector('.dice');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
@@ -15,14 +17,24 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // rolling dice functionallity
 
 btnRoll.addEventListener('click', function () {
   //1 generate random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+
   //2display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
@@ -30,10 +42,23 @@ btnRoll.addEventListener('click', function () {
   if (dice != 1) {
     //add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; //change latter
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch to next player
-    currentScore = 0;
-    current1El.textContent = currentScore;
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  //1add current score to active player
+  scores[activePlayer] += currentScore;
+
+  //scores[1]=scores[1]+currentScore
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  //2check score is already 100
+  //finish game
+  //switch next player
+  switchPlayer();
 });
