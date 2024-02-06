17f5ef07b31015f56fe85ddab906ae1a92bf310b
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-02-06 20:18:22 +0200

    smf

--- complete-javascript-course-jonasschmedtmann/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/08-Behind-the-Scenes/starter/script.js
@@ -1,45 +1,97 @@
 'use strict';
 
 //variable hoisting
-console.log(me);
-// console.log(job);
-// console.log(year);
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
 
-var me = 'Jonas';
-let job = 'teacher';
-const year = 1991;
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
 
-//function hoisting
+// //function hoisting
 
-console.log(addDecl(2, 3));
-// console.log(addExpr(2, 3));
-console.log(addArrow);
-// console.log(addArrow(2, 3));
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow);
+// // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-  return a + b;
-}
+// function addDecl(a, b) {
+//   return a + b;
+// }
 
-const addExpr = function (a, b) {
-  return a + b;
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// //Example
+// console.log(numProducts);
+// if (!numProducts) deleteshopingCart();
+
+// var numProducts = 10;
+
+// function deleteshopingCart() {
+//   console.log('all products delted!');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+////this keyword practise///
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// calcAgeArrow(1991);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
+
+//////Regular vs arrow/////
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
 };
 
-var addArrow = (a, b) => a + b;
-
-//Example
-console.log(numProducts);
-if (!numProducts) deleteshopingCart();
-
-var numProducts = 10;
-
-function deleteshopingCart() {
-  console.log('all products delted!');
-}
-
-var x = 1;
-let y = 2;
-const z = 3;
-
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+jonas.greet();
