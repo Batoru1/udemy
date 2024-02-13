ab2054c1734601295138af89b155426194e672b6
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-02-13 06:23:47 +0200

    default values destructuring

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/script.js
@@ -57,7 +57,13 @@ console.log(main, secondary);
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
-const nested = [
-  [2, 4],
-  [5, 6],
-];
+//nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
