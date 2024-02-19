4fab62525a0286914ebc1c8ee89724957a7199bc
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-02-19 18:53:13 +0200

    spread operator

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -313,3 +313,32 @@ const {
 } = books[0];
 
 // console.log(bookRating);
+
+////////////////////////////////////////////
+////////////the spread operator////////////
+//////////////////////////////////////////
+
+// 3.1
+// Each book object has the author property, which stores an array of strings (author names) if there are multiple authors, or a single string (author name) if there is just one author.
+
+// Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).
+
+const bookAuthors = [...books[0].author, ...books[1].author];
+// console.log(bookAuthors);
+
+// 3.2
+// Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.
+
+// Example
+// Code:
+
+// spellWord('JavaScript')
+// Expected output:
+
+// "J a v a S c r i p t"
+
+const spellWord = function (str) {
+  console.log(...str);
+};
+
+// spellWord('bam');
