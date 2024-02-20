ff5cd0fe1bd620211f782349328d45bdf6289ee8
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-02-20 06:47:30 +0200

    rest syntax,args, params

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/script.js
@@ -51,8 +51,11 @@ const restaurant = {
 ///////////rest pattern and parameters//////////////
 ///////////////////////////////////////////////////
 
+//1)Detructuring
+
 //SPREAD because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
+console.log(arr);
 
 //REST, because on LEFT side of =
 const [a, b, ...others] = [1, 2, 3, 4, 5];
@@ -64,6 +67,22 @@ const [pizza, , risotto, ...otherFood] = [
 ];
 console.log(pizza, risotto, otherFood);
 
+//objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//2) functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    return (sum += numbers[i]);
+  }
+};
+
+console.log(add(2, 3));
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
 /////////////////////////////////////////////////////////
 /////////////////spread operator/////////////////////////
 ////////////////////////////////////////////////////////
@@ -96,11 +115,11 @@ console.log(pizza, risotto, otherFood);
 // console.log(...str);
 // // console.log(`${...str} Schmedtman`);
 
-// //real world exmp
+// //real world exmp PASSING MLTIPLE arguments into  a FUNCTION
 // const ingredients = [
-//   // prompt("Let's make pasta! Ingredient 1?"),
-//   // prompt('Ingredient 2?'),
-//   // prompt('Ingredient 3?'),
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
 // ];
 // console.log(ingredients);
 
