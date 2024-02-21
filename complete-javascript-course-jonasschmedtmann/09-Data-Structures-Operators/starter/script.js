3abf8d877ce512865d03041c2f9d965878fcc714
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-02-21 06:51:03 +0200

    rest params

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/script.js
@@ -45,6 +45,14 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    // console.log(mainIngredient);
+    // console.log(otherIngredients);
+    console.log(
+      `this is a pizza containing ${mainIngredient}, ${otherIngredients}`
+    );
+  },
 };
 
 ////////////////////////////////////////////////////
@@ -73,16 +81,24 @@ const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
 
 //2) functions
-const add = function (...numbers) {
+const add = function (...numbers) /*contract into arr again!!!!*/ {
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) {
-    return (sum += numbers[i]);
+    sum += numbers[i];
   }
+  // return sum;
+  console.log(sum);
 };
 
-console.log(add(2, 3));
+add(2, 3);
 add(5, 3, 7, 2);
 add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x); //expand arrr!!!!!!!!!
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
 /////////////////////////////////////////////////////////
 /////////////////spread operator/////////////////////////
 ////////////////////////////////////////////////////////
