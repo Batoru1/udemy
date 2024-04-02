2126b06b6fa1d9689dde64a005b525c69123ab98
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-04-02 20:30:10 +0300

    smth

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -238,7 +238,10 @@ const books = [
 // }
 // Write a function called hasExamplesInJava that takes a book object from the books array as an argument. This function should return true if the book uses Java, or a string 'no data available' if it uses other language or no programming language at all.
 
-const hasExamplesInJava = books.programmingLanguage('Java');
+function hasExamplesInJava(book) {
+  return book.programmingLanguage === 'Java' || 'no data';
+}
+console.log(hasExamplesInJava(books[0]));
 
 // Use short-circuiting.
 
