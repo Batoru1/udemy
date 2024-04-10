df8c0d89d70460ffa9abec07f8418c721a28883e
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-04-10 06:40:08 +0300

    nullish coalescing ??

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -224,6 +224,17 @@ const books = [
   },
 ];
 
+////////////////////////////////////////////////////////
+/////////The Nullish Coalescing Operator (??)///////////
+//////////////////////////////////////////////////////
+
+//6.1
+// There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, and log a string to the console in this format: "${title}" provides no data about its online content.
+//Solution:
+for (let i = 0; i < books.length; i++) {
+  const element = books[i];
+}
+
 ////////////////////////////////////////////////////////////////////////////Short Circuiting ($$ and ||)/////////////////////
 //////////////////////////////////////////////////////////////
 
