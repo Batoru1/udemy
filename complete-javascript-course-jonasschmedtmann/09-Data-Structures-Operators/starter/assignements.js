17dae1499e79905d108ed790d25ea1a6bf6c9efa
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-05-15 06:09:20 +0300

    Sets exercises

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -228,59 +228,69 @@ const books = [
 ///////////////////////////SETS//////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
-/*12.1
-Below is the allKeywords variable, which stores an empty array. Loop over the books array, and fill the allKeywords array with the keywords coming from the keywords property of each book object. The allKeywords array should have just one level (no nested arrays).
+// /*12.1
+// Below is the allKeywords variable, which stores an empty array. Loop over the books array, and fill the allKeywords array with the keywords coming from the keywords property of each book object. The allKeywords array should have just one level (no nested arrays).
 
-Use whatever loop and methods you want. You can also use the spread syntax. In the end, the allKeywords array should look more or less like this: ['computer science', 'programming', 'algorithms', 'data structures', ...].
+// Use whatever loop and methods you want. You can also use the spread syntax. In the end, the allKeywords array should look more or less like this: ['computer science', 'programming', 'algorithms', 'data structures', ...].
 
-const allKeywords = [];
-*/
-const allKeywords = [];
+// const allKeywords = [];
+// */
+// const allKeywords = [];
 
-for (const book of books) {
-  // console.log(book);
-  for (const key of book.keywords) {
-    // console.log(key);
-    allKeywords.push(key);
-  }
-}
+// //my way
+// for (const book of books) {
+//   // console.log(book);
+//   for (const key of book.keywords) {
+//     // console.log(key);
+//     allKeywords.push(key);
+//   }
+// }
 
-console.log(allKeywords);
+// //2nd way
+// // for (const book of books) {
+// //   allKeywords.push(...book.keywords);
+// // }
 
-/*12.2
-The allKeyword array contains duplicates. Remove them by creating a Set out of that array. Assign the newly created set to the uniqueKeywords variable. 
-*/
+// console.log(allKeywords);
 
-const uniqueKeywords = new Set(allKeywords);
-console.log(uniqueKeywords);
+// /*12.2
+// The allKeyword array contains duplicates. Remove them by creating a Set out of that array. Assign the newly created set to the uniqueKeywords variable.
+// */
 
-/*12.3
-Add two more keywords to the uniqueKeywords set, for example, 'coding' and 'science'.
- */
-uniqueKeywords.add('coding');
-uniqueKeywords.add('science');
-console.log(uniqueKeywords);
+// const uniqueKeywords = new Set(allKeywords);
+// console.log(uniqueKeywords);
 
-/*12.4
-Delete 'business' from the uniqueKeywords set. 
-*/
+// /*12.3
+// Add two more keywords to the uniqueKeywords set, for example, 'coding' and 'science'.
+//  */
+// uniqueKeywords.add('coding');
+// uniqueKeywords.add('science');
+// console.log(uniqueKeywords);
 
-uniqueKeywords.delete('business');
-console.log(uniqueKeywords);
+// /*12.4
+// Delete 'business' from the uniqueKeywords set.
+// */
 
-/*12.5
-Create an array out of the uniqueKeywords set, and assign it to the uniqueKeywordsArr variable.
-*/
+// uniqueKeywords.delete('business');
+// console.log(uniqueKeywords);
 
-const uniqueKeywordsArr = [...new Set(uniqueKeywords)];
-console.log(uniqueKeywordsArr);
+// /*12.5
+// Create an array out of the uniqueKeywords set, and assign it to the uniqueKeywordsArr variable.
+// */
 
-/* 12.6
-Delete all items from the uniqueKeywords set.
-*/
+// //my way
+// const uniqueKeywordsArr = [...new Set(uniqueKeywords)];
 
-uniqueKeywords.clear();
-console.log(uniqueKeywords);
+// //2nd way
+// // const uniqueKeywordsArr = [...uniqueKeywords];
+
+// console.log(uniqueKeywordsArr);
+// /* 12.6
+// Delete all items from the uniqueKeywords set.
+// */
+
+// uniqueKeywords.clear();
+// console.log(uniqueKeywords);
 
 ///////////////////////////////////////////////////////////////
 //////////Looping Objects:Object Keys, Values and Entries/////
