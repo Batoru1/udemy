874b7367744252ecd9340404cb1535a335781fd0
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-05-31 06:37:54 +0300

    coding challange 3

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -854,6 +854,22 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
+/*Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+⚽
+[FIRST HALF] 17:
+GOAL*/
+
 //////////////////////////////////////////////////////////////
 ///////////////////Coding Challenge #2////////////////////////
 //////////////////////////////////////////////////////////////
