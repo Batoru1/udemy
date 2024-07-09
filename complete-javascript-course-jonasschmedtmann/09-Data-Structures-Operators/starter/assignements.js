59c561050cb3b685c5e907801dab5155a15db8be
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-07-09 20:02:23 +0300

    coding challange 4

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -1047,12 +1047,15 @@ and convert them to camelCase.
 The input will come from a textarea inserted into the DOM (see code below to
 insert the elements), and conversion will happen when the button is pressed.
 Test data (pasted to textarea, including spaces):
+
 underscore_case
 first_name
 Some_Variable
 calculate_AGE
 delayed_departure
+
 Should produce this output (5 separate console.log outputs):
+
 underscoreCase
 firstName✅
 ✅✅
@@ -1060,6 +1063,7 @@ someVariable
 calculateAge✅✅✅
 ✅✅✅✅
 delayedDeparture✅✅✅✅✅
+
 Hints:
 §Remember which character defines a new line in the textarea 😉
 §The solution only needs to work for a variable made out of 2 words, like a_b
@@ -1069,9 +1073,15 @@ name conversion working 😉
 This challenge is difficult on purpose, so start watching the solution in case
 you're stuck. Then pause and continue!
 Afterwards, test with your own test data!
-GOOD LUCK 😀
+GOOD LUCK 😀*/
 document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));*/
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows);
+});
 
 ///////////////////////////////////////////////////////////
 //////////////////Coding Challange #3///////////////////////
