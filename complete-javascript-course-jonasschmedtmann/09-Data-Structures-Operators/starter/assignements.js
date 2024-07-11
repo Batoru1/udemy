e747bfcde0a422226489424d5d861dbbced22927
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-07-11 19:05:01 +0300

    more exercises

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -1045,6 +1045,17 @@ const game = {
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+console.log(flights.split('+'));
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('Delayed') ? '🫥' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${from} ${to} (${time.replace(':', 'h')})`;
+  console.log(output);
+}
+
 ///////////////////////////////////////////////////////////
 //////////////////Coding Challange #4///////////////////////
 ////////////////////////////////////////////////////////////
