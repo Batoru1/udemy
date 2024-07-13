ab399d4e94d2d45df949f9f094520f89a9675ba7
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-07-13 08:15:25 +0300

    data structures finished

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -1045,14 +1045,21 @@ const game = {
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-console.log(flights.split('+'));
+// console.log(flights.split('+'));
+
+//refractoring
+const getCode = str => str.slice(0, 3).toUpperCase();
 
 for (const flight of flights.split('+')) {
   const [type, from, to, time] = flight.split(';');
-  const output = `${type.startsWith('Delayed') ? '🫥' : ''}${type.replaceAll(
+
+  const output = `${type.startsWith('_Delayed') ? '🫥' : ''}${type.replaceAll(
     '_',
     ' '
-  )} ${from} ${to} (${time.replace(':', 'h')})`;
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(50);
   console.log(output);
 }
 
