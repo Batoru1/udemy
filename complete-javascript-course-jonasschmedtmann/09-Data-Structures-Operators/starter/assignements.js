82f8208fc57763761aa89f1fa5c5283b22310fce
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-07-30 21:21:59 +0300

    mhm

--- complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
+++ complete-javascript-course-jonasschmedtmann/09-Data-Structures-Operators/starter/assignements.js
@@ -228,6 +228,35 @@ const books = [
 ///////////////////////////REPEAT///////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
 
+// Working with Strings - Part 1
+// 15.1
+// Take the ISBN property of the first book from the books array, and log to the console characters at index 6, 4, 9 and 8. Use bracket notation to access individual characters.
+
+// show example solution
+//  {...}
+// 15.2
+// Below is the quote variable that stores a string. Find the index of the word 'chess', and log it to the console.
+
+// const quote = 'A computer once beat me at chess, but it was no match for me at kick boxing';
+// show example solution
+//  {...}
+// 15.3
+// Extract the word "boxing" from the same quote string, and log it to the console.
+
+// show example solution
+//  {...}
+// 15.4
+// Some authors are noted as "(Contributor)", for example "Julie Sussman (Contributor)". Create a function called isContributor that takes an author's name as an argument, and returns either true (if he's a contributor) of false (if he's not a contributor). The string "(Contributor)" is always the last part of the author's name string.
+
+// Example 1
+// Example 2
+// Code:
+
+// isContributor('Julie Sussman (Contributor)');
+// Expected output:
+
+// true
+
 // // Maps: Iteration
 // // 14.1
 // // Convert the first book object from the books array into a Map, and assign it to a firstBookMap variable.
