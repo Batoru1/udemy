b1ed50b6bbfdfe6370d36a3cdad291ead583758a
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-08-08 06:33:27 +0300

    first class and higher order functions

--- complete-javascript-course-jonasschmedtmann/10-Functions/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/10-Functions/starter/script.js
@@ -86,10 +86,40 @@ console.log(jonas);
 // passenger = jonas;
 
 //Is the same as doing THIS...
-const flightNum = flight;
-const passenger = jonas;
+// const flightNum = flight;
+// const passenger = jonas;
+
+//function that will accept a person and change their's passport number
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
 
 //summary
 //passing a primitive type to a function is really just the same as creating a copy like this, outside of the function, so the value is simply copied
 
 //on the other hand, when we pass an object to a function, it is just like copying an object like THIS and so whatever we change in a copy will also hapen in the original. Be careful because if objects change when you change them outside of function, this could have big consequences for large code bases. NEXT exmp will tackle this
+
+//Pssing by VALUE, passing by REFERENCE
+
+//JS doesnt have passing by reference, only passing by value, even thought it looks like passing by reference
+//we pass a reference to the function, but we do not pass by reference
+
+/////FIRST-CLAS and HIGHER-ORDER functions////////////////
+
+//first-class functions - first citizens- they are simply values. They act like this, because they are just another type of object in Java-Script.
+
+//we can store functions in variables or object properties
+
+// we can also pass functions as arguments to other functions(adding event listeners or event handlers to DOM methods)
+
+//we can also return a function from another function
+
+//functions are like objects and objets have methods so functions also have METHODS-methods that we can call on functions
+
+//first class functions lets us write HIGHER-ORDER functions
+//---HIGHER-ORDER function is 1) either a function that receives another function as an argument 2) or a function that returns a new function  ---
+//a function that is passed in(another function) is a CALL-BACK function, that's because the callback function will be called latter by the higher-order function(most obvious example is addEventListener  functions)
+//Functions that return another function
