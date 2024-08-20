ff526c913f97745012da017367f1204c4ea50764
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-08-20 06:10:09 +0300

    yes

--- complete-javascript-course-jonasschmedtmann/10-Functions/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/10-Functions/starter/script.js
@@ -679,21 +679,30 @@ B*** setting this.keyword
 //////////////140 CODING CHALLANGE 2/////////////////////
 ////////////////////////////////////////////////////////
 
-/*This is more of a thinking challenge than a coding challenge 🤓
-Your tasks:
-1. Take the IIFE below and at the end of the function, attach an event listener that
-changes the color of the selected h1 element ('header') to blue, each time
-the body element is clicked. Do not select the h1 element again! -use header
-2. And now explain to yourself (or someone around you) why this worked! Take all
-the time you need. Think about when exactly the callback function is executed,
-and what that means for the variables involved in this example.*/
+// /*This is more of a thinking challenge than a coding challenge 🤓
+// Your tasks:
+// 1. Take the IIFE below and at the end of the function, attach an event listener that
+// changes the color of the selected h1 element ('header') to blue, each time
+// the body element is clicked. Do not select the h1 element again! -use header
+// 2. And now explain to yourself (or someone around you) why this worked! Take all
+// the time you need. Think about when exactly the callback function is executed,
+// and what that means for the variables involved in this example.*/
 
-(function () {
-  const header = document.querySelector('h1');
-  header.style.color = 'red';
-  header.addEventListener('click', function () {
-    header.style.color = 'blue';
-  });
-})();
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+//   header.addEventListener('click', function () {
+//     header.style.color = 'blue';
+//   });
+// })();
 
-//2 it worked, because thecall-back(eventlistener) function had access to all the variabnles inside header, necause of closure
+// //2 it worked, because thecall-back(eventlistener) function had access to all the variabnles inside header, necause of closure
+///jonas exmpl
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+
+//   document.querySelector('body').addEventListener('click', function () {
+//     header.style.color = 'blue';
+//   });
+// })();
