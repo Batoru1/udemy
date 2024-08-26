bc9e70f6dda95262c11637a30afd22ac3595454d
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-08-26 06:13:57 +0300

    forEach() with Map, Set

--- complete-javascript-course-jonasschmedtmann/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-jonasschmedtmann/11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -170,66 +164,90 @@ const currencies = new Map([
 ///////145.LOOPING ARRAYS: forEach///////////
 /////////////////////////////////////////////
 
-//lets say that we want to loop over this arr in order to print a message for each movement in this bank account
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// //lets say that we want to loop over this arr in order to print a message for each movement in this bank account
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-//for of
-for (const movement of movements) {
-  if (movement > 0) {
-    console.log(`You deposited ${movement}`);
-  } else {
-    console.log(`You withdrew ${Math.abs(movement)}`); //absolute value to remove the -
-  }
-}
+// //for of
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(movement)}`); //absolute value to remove the -
+//   }
+// }
 
-//!!.forEach(callback function(){})!! - so technically .forEach() is a higher order function, which requires a callback function in order to tell it what to do
-//when will forEach() calback the function? forEach() loops over the array and in each itteration it will execute the callback function. also as the forEach() method calls this callback function and in each itteration it will pass in the !current element of the array as an argument! movement in this case
+// //!!.forEach(callback function(){})!! - so technically .forEach() is a higher order function, which requires a callback function in order to tell it what to do
+// //when will forEach() calback the function? forEach() loops over the array and in each itteration it will execute the callback function. also as the forEach() method calls this callback function and in each itteration it will pass in the !current element of the array as an argument! movement in this case
 
-//access to current index in forEach!!
-//how it works?
-//it is the forEach() method that calls the callback function in each iteration
-//and as it calls this function it also passes in the current element of the array
-//and actually it also passes 1)the current element of the array 2)! the index 3) and the entire array that we are looping!
-// they -1) 2), 3) can be specified in our parameters list
-// the names of the params don't matter - what matters is the order:
-//ALWAYS !1st param - current element, !2nd param - current index, !3d param - entire array that we are looping over
-//i+1 because index starts at 0
-console.log('----FOREACH----');
+// //access to current index in forEach!!
+// //how it works?
+// //it is the forEach() method that calls the callback function in each iteration
+// //and as it calls this function it also passes in the current element of the array
+// //and actually it also passes 1)the current element of the array 2)! the index 3) and the entire array that we are looping!
+// // they -1) 2), 3) can be specified in our parameters list
+// // the names of the params don't matter - what matters is the order:
+// //ALWAYS !1st param - current element, !2nd param - current index, !3d param - entire array that we are looping over
+// //i+1 because index starts at 0
+// console.log('----FOREACH----');
 
-movements.forEach(function (mov, i, arr) {
-  if (mov > 0) {
-    console.log(`Movement ${i + 1} You deposited ${mov}`);
-  } else {
-    console.log(`Movement ${i + 1} You withdrew ${Math.abs(mov)}`); //absolute value to remove the -minus
-  }
-});
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1} You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1} You withdrew ${Math.abs(mov)}`); //absolute value to remove the -minus
+//   }
+// });
 
-//at itteration zero
-//0: function(200)
-//at 1
-//1: function(450)
-//2: function(400)
-//...
+// //at itteration zero
+// //0: function(200)
+// //at 1
+// //1: function(450)
+// //2: function(400)
+// //...
 
-// !we use a callback function to tell another higher order function exactly what it should do!
-//in the above example we tell forEach() that in each itteration it should log one of the 2 strings to the console
-//!we give the forEach() method instructions by giving it a callback function, which conatains these instructions!
+// // !we use a callback function to tell another higher order function exactly what it should do!
+// //in the above example we tell forEach() that in each itteration it should log one of the 2 strings to the console
+// //!we give the forEach() method instructions by giving it a callback function, which conatains these instructions!
 
-//!what if we actually needed access to a counter variable here?
+// //!what if we actually needed access to a counter variable here?
 
-//So just like we can access the current index of the array in the for of loop
-// movements.forEach(function (movement) {
-//!.entries() is just another array method - it returns array of arrays,which in the first position contains i-current index, j-value itself or current array element (in this case we call j - movment) !
-//i+1 because index starts at 0
-console.log('----ENTRIES----');
+// //So just like we can access the current index of the array in the for of loop
+// // movements.forEach(function (movement) {
+// //!.entries() is just another array method - it returns array of arrays,which in the first position contains i-current index, j-value itself or current array element (in this case we call j - movment) !
+// //i+1 because index starts at 0
+// console.log('----ENTRIES----');
 
-for (const [i, movement] of movements.entries())
-  if (movement > 0) {
-    console.log(`Movement ${i + 1} You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`); //absolute value to remove the -minus
-  }
+// for (const [i, movement] of movements.entries())
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1} You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`); //absolute value to remove the -minus
+//   }
 
-//!!When use forEach() when forof loop?
-//fundamental diffrence between the two of them: is that you !cannot break out of a forEach() loop!. So the continue and break statements don't work in a forEach() loop so instead - forEach() will always loop over the entire array and ther is nothing you can do about it.
-//So if you really need to !break out of a loop! then you need to use the forof loop
+// //!!When use forEach() when forof loop?
+// //fundamental diffrence between the two of them: is that you !cannot break out of a forEach() loop!. So the continue and break statements don't work in a forEach() loop so instead - forEach() will always loop over the entire array and ther is nothing you can do about it.
+// //So if you really need to !break out of a loop! then you need to use the forof loop
+
+/////////////////////////////////////////////////////146 forEach() with Maps and Sets////
+////////////////////////////////////////////
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]); //1st- key, 2nd - value
+
+// //Map
+// // foreEach() Map params:1) current value of mapS(in the current iteration), 2) key 3) the entire map that is being looped over
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// //Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${value}: ${value}`);
+// });
+// // a Set doesn't have keys nor indexes
+// //_ - underscore means - !throwaway variable - variable that is completely unecessery
